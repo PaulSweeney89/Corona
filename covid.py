b0e43929ca91fd529fd34857ef8fbc31e018ffa7
paulsweeney89 <pl.sweeney3@gmail.com>
2020-10-06 23:37:36 +0100

    updated formatting of graphs for legibility

--- covid.py
+++ covid.py
@@ -12,63 +12,63 @@ df = pd.read_csv("http://opendata-geohive.hub.arcgis.com/datasets/d8eb52d5627341
 
 today = date.today()
 
-df['Date'] = pd.to_datetime(df['Date'], unit='ms')					# csv file date format was changed to milliseconds
+df['Date'] = pd.to_datetime(df['Date']) #unit='ms')					# csv file date format was changed to milliseconds
 df['Datestr'] = df['Date'].dt.strftime('%d/%m')
 
-plt.bar(x=df["Datestr"], height=df["TotalConfirmedCovidCases"], color='b')
-plt.bar(x=df["Datestr"], height=df["ConfirmedCovidCases"], color='c')
-plt.xticks(df["Datestr"], rotation = '90')
+plt.bar(x=df["Date"], height=df["TotalConfirmedCovidCases"], color='b')
+plt.bar(x=df["Date"], height=df["ConfirmedCovidCases"], color='c')
+#plt.xticks(df["Datestr"], rotation = '90')
 plt.grid(which='major', axis='y', linestyle='dotted', alpha=0.8)
 plt.title("Ireland Covid 19 Total Confirmed Cases"+"\n"+str(today))
-for i in range(len(df)):
-    t = df.loc[i, "TotalConfirmedCovidCases"]
-    c = df.loc[i, "ConfirmedCovidCases"]
-    plt.text(df.loc[i, "Datestr"], df.loc[i, "TotalConfirmedCovidCases"] + 0.25, t, size=8, fontweight='bold')
-    plt.text(df.loc[i, "Datestr"], df.loc[i, "ConfirmedCovidCases"] + 0.25, c, size=8, fontweight='bold') 
+#for i in range(len(df)):
+    #t = df.loc[i, "TotalConfirmedCovidCases"]
+    #c = df.loc[i, "ConfirmedCovidCases"]
+    #plt.text(df.loc[i, "Datestr"], df.loc[i, "TotalConfirmedCovidCases"] + 0.25, t, size=8, fontweight='bold')
+    #plt.text(df.loc[i, "Datestr"], df.loc[i, "ConfirmedCovidCases"] + 0.25, c, size=8, fontweight='bold') 
 plt.show()
 
-plt.bar(x=df["Datestr"], height=df["TotalCovidDeaths"], color='r')
-plt.xticks(df["Datestr"], rotation = '90')
+plt.bar(x=df["Date"], height=df["TotalCovidDeaths"], color='r')
+#plt.xticks(df["Datestr"], rotation = '90')
 plt.grid(which='major', axis='y', linestyle='dotted', alpha=0.8)
 plt.title("Ireland Covid 19 Total Confirmed Deaths"+"\n"+str(today))
-for i in range(len(df)):
-    t = df.loc[i, "TotalCovidDeaths"]
-    plt.text(df.loc[i, "Datestr"], df.loc[i, "TotalCovidDeaths"] + 0.25, t, size=8, fontweight='bold')
+#for i in range(len(df)):
+    #t = df.loc[i, "TotalCovidDeaths"]
+    #plt.text(df.loc[i, "Datestr"], df.loc[i, "TotalCovidDeaths"] + 0.25, t, size=8, fontweight='bold')
 plt.show()
 
-plt.plot(df["Datestr"], df["ConfirmedCovidCases"], marker='o', markersize=3, color="blue")
-plt.xticks(df["Datestr"], rotation = '90')
+plt.plot(df["Date"], df["ConfirmedCovidCases"], marker='o', markersize=1, color="blue")
+#plt.xticks(df["Datestr"], rotation = '90')
 plt.grid(which='major', axis='y', linestyle='dotted', alpha=0.8)
 plt.title("Ireland Covid 19 Daily Confirmed New Cases"+"\n"+str(today))
-for i in range(len(df)):
-    t = df.loc[i, "ConfirmedCovidCases"]
-    plt.text(df.loc[i, "Datestr"], df.loc[i, "ConfirmedCovidCases"] + 0.25, t, size=8)
+#for i in range(len(df)):
+    #t = df.loc[i, "ConfirmedCovidCases"]
+    #plt.text(df.loc[i, "Datestr"], df.loc[i, "ConfirmedCovidCases"] + 0.25, t, size=8)
 plt.show()
 
-plt.plot(df["Datestr"], df["TotalConfirmedCovidCases"], marker='o', markersize=3, color="red")
-plt.plot(df["Datestr"], df["ConfirmedCovidRecovered"], marker='o', markersize=3, color="green")
-plt.xticks(df["Datestr"], rotation = '90')
+plt.plot(df["Date"], df["TotalConfirmedCovidCases"], marker='o', markersize=1, color="red")
+plt.plot(df["Date"], df["ConfirmedCovidRecovered"], marker='o', markersize=1, color="green")
+#plt.xticks(df["Datestr"], rotation = '90')
 plt.grid(which='major', axis='y', linestyle='dotted', alpha=0.8)
 plt.title("Ireland Covid 19 Confirmed Cases & Confirmed Recovered"+"\n"+str(today))
-for i in range(len(df)):
-    t = df.loc[i, "TotalConfirmedCovidCases"]
-    plt.text(df.loc[i, "Datestr"], df.loc[i, "TotalConfirmedCovidCases"] + 0.25, t, size=8)
-for j in range(len(df)):
-    t = df.loc[j, "ConfirmedCovidRecovered"]
-    plt.text(df.loc[j, "Datestr"], df.loc[j, "ConfirmedCovidRecovered"] + 0.25, t, size=8, rotation = '45')
+#for i in range(len(df)):
+    #t = df.loc[i, "TotalConfirmedCovidCases"]
+    #plt.text(df.loc[i, "Datestr"], df.loc[i, "TotalConfirmedCovidCases"] + 0.25, t, size=8)
+#for j in range(len(df)):
+    #t = df.loc[j, "ConfirmedCovidRecovered"]
+    #plt.text(df.loc[j, "Datestr"], df.loc[j, "ConfirmedCovidRecovered"] + 0.25, t, size=8, rotation = '45')
 plt.show()
 
 df["x"] = df["TotalConfirmedCovidCases"] - df["ConfirmedCovidRecovered"]
-plt.bar(x=df["Datestr"], height=df["x"], color='b')
-plt.xticks(df["Datestr"], rotation = '90')
+plt.bar(x=df["Date"], height=df["x"], color='b')
+#plt.xticks(df["Datestr"], rotation = '90')
 plt.grid(which='major', axis='y', linestyle='dotted', alpha=0.8)
 plt.title("Ireland Covid 19 Total - Recovered Cases"+"\n"+str(today))
-for i in range(len(df)):
-    t = df.loc[i, "x"]
-    plt.text(df.loc[i, "Datestr"], df.loc[i, "x"] + 0.25, t, size=8)
+#for i in range(len(df)):
+    #t = df.loc[i, "x"]
+    #plt.text(df.loc[i, "Datestr"], df.loc[i, "x"] + 0.25, t, size=8)
 plt.show()
 
-plt.plot(df["TotalConfirmedCovidCases"],df["ConfirmedCovidCases"], marker='o', markersize=3, color="green")
+plt.plot(df["TotalConfirmedCovidCases"],df["ConfirmedCovidCases"], marker='o', markersize=1, color="green")
 plt.xlabel("Total Confirmed Covid Cases")
 plt.ylabel("Daily New Confirmed Covid Cases")
 plt.title("Ireland Covid 19"+"\n"+str(today))
